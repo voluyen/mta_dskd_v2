3deea8804d86c24e4a004c23be86714e98d39685
unknown <voluyenpr@gmail.com>
2026-05-23 16:23:38 +0700

    .

--- run.sh
+++ run.sh
@@ -47,8 +47,8 @@ export NCCL_P2P_DISABLE=1
 
 ALL_SCRIPTS=(
     # "scripts/dolly/gpt2-120M/run_dskdv2_eta.sh"
-    # "scripts/dolly/gpt2-120M/run_mta_dskdv2_eta.sh"
-    # "scripts/dolly/gpt2-340M/run_mta_dskdv2_eta.sh"
+    "scripts/dolly/gpt2-120M/run_mta_dskdv2_eta.sh"
+    "scripts/dolly/gpt2-340M/run_mta_dskdv2_eta.sh"
     "scripts/dolly/gpt2-1.5B/run_mta_dskdv2_eta.sh"
     "scripts/dolly/opt-2.7B/run_mta_dskdv2_eta.sh"
     "scripts/dolly/tinyllama-1.1B/run_mta_dskdv2_eta.sh"
